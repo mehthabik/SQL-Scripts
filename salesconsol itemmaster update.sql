05bc9f076d6be2dfa1ead20bf6596ddd52d0a5c5
Mehthab Khan <mehthabik123@gmail.com>
2026-04-15 17:19:41 +0400

    Add files via upload
    Salesconsol update for hierarchies and other fields from Dim_Itemmaster

---
+++ salesconsol itemmaster update.sql
@@ -0,0 +1,74 @@
+
+UPDATE sc
+SET sc.Description = stg.PRODUCTNAME
+FROM SalesConsol sc
+left JOIN Dim_ItemMaster stg
+ON sc.company = stg.company
+AND sc.itemid = stg.itemnumber
+where sc.Date>='2025-02-01'
+
+UPDATE sc
+SET sc.Department = stg.LTCATEGORY1
+FROM SalesConsol sc
+left JOIN Dim_ItemMaster stg
+ON sc.company = stg.company
+AND sc.itemid = stg.itemnumber
+where sc.Date>='2025-02-01'
+
+UPDATE sc
+SET sc.SubDepartment = stg.LTCATEGORY2
+FROM SalesConsol sc
+left JOIN Dim_ItemMaster stg
+ON sc.company = stg.company
+AND sc.itemid = stg.itemnumber
+where sc.Date>='2025-02-01'
+
+UPDATE sc
+SET sc.Class = stg.LTCATEGORY3
+FROM SalesConsol sc
+left JOIN Dim_ItemMaster stg
+ON sc.company = stg.company
+AND sc.itemid = stg.itemnumber
+where sc.Date>='2025-02-01'
+
+UPDATE sc
+SET sc.SubClass = stg.LTCATEGORY4
+FROM SalesConsol sc
+left JOIN Dim_ItemMaster stg
+ON sc.company = stg.company
+AND sc.itemid = stg.itemnumber
+where sc.Date>='2025-02-01'
+
+UPDATE sc
+SET sc.Brand = stg.BRAND
+FROM SalesConsol sc
+left JOIN Dim_ItemMaster stg
+ON sc.company = stg.company
+AND sc.itemid = stg.itemnumber
+where sc.Date>='2025-02-01'
+
+UPDATE sc
+SET sc.Supplier = stg.VENDORID
+FROM SalesConsol sc
+left JOIN Dim_ItemMaster stg
+ON sc.company = stg.company
+AND sc.itemid = stg.itemnumber
+where sc.Date>='2025-02-01'
+
+UPDATE sc
+SET sc.SupplierName = stg.VENDORNAME
+FROM SalesConsol sc
+left JOIN Dim_ItemMaster stg
+ON sc.company = stg.company
+AND sc.itemid = stg.itemnumber
+where sc.Date>='2025-02-01'
+
+UPDATE sc
+SET sc.Stype = stg.ITEMGROUPNAME
+FROM SalesConsol sc
+left JOIN Dim_ItemMaster stg
+ON sc.company = stg.company
+AND sc.itemid = stg.itemnumber
+where sc.Date>='2025-02-01'
+
+---------------------------------------
